185ee1d94eaac716dc9af100c532912e795946af
Ahmadkashif <ahmadkashif983@gmail.com>
2020-10-05 19:50:57 +0500

    q 1 4 2P

--- Main.py
+++ Main.py
@@ -2,9 +2,11 @@
 ## This is the main Assignment runner function.
 #
 from Question__1_ import question1
+from Question__2_ import question2
 from Question__4_ import question4
-question1Input = "qwertyuiopasdfghjklzxcvbnm";
 
+question1Input = "qwertyuiopasdfghjklzxcvbnm";
+question2Input = [[33, 120], [40], [30, 56, 25], [33, 120], [33], [40]];
 question4Input1 = {
     'a':1,
     'b':2,
@@ -18,7 +20,9 @@ question4Input2 = {
     'b':3,
     'c':4
 }
-print(">>>>>question 1 output<<<<")
+print(">>>>>question 1 output<<<<<")
 print(question1(question1Input))
-print(">>>>>question 4 output<<<<")
+print(">>>>>question 2 output<<<<<")
+print(question2(question2Input))
+print(">>>>>question 4 output<<<<<")
 print(question4(question4Input1,question4Input2))
